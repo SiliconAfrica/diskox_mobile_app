38d71ef13be4a650288bebdaf64a4ad981f24590
izuutech <izutechsofficial@gmail.com>
2023-09-25 14:08:35 +0100

    fetch referrals count and list

--- src/pages/profile/pages/referrals/totalReferrals.tsx
+++ src/pages/profile/pages/referrals/totalReferrals.tsx
@@ -1,7 +1,26 @@
+import { useQuery } from "react-query";
 import Box from "../../../../components/general/Box";
 import CustomText from "../../../../components/general/CustomText";
+import httpService from "../../../../utils/httpService";
+import { URLS } from "../../../../services/urls";
+import { useState } from "react";
 
 export default function TotalReferrals() {
+  const [total, setTotal] = useState<number>(0);
+  const { isLoading } = useQuery(
+    ["referrals_count"],
+    () => httpService.get(`${URLS.COUNT_REFERRALS}`),
+    {
+      onSuccess: (data) => {
+        if (data.data.code === 1) {
+          setTotal(data.data.number_of_referrals);
+        }
+      },
+      onError: (error: any) => {
+        alert(error.message);
+      },
+    }
+  );
   return (
     <Box
       backgroundColor="white"
@@ -26,7 +45,7 @@ export default function TotalReferrals() {
           Total Referrals
         </CustomText>
         <CustomText variant="subheader" color="black">
-          0
+          {total}
         </CustomText>
       </Box>
     </Box>
