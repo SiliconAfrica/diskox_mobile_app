e5b66edfc0a8783eceec1ac535eea8c3b11af246
izuutech <izutechsofficial@gmail.com>
2023-11-10 19:33:41 +0100

    community add c to username

--- src/components/community/CommunityListCard.tsx
+++ src/components/community/CommunityListCard.tsx
@@ -1,57 +1,94 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import Box from '../general/Box'
-import PrimaryButton from '../general/PrimaryButton'
-import CustomText from '../general/CustomText'
-import { useNavigation } from '@react-navigation/native'
-import { PageType } from '../../pages/login'
-import { ICommunity } from '../../models/Community'
-import { Image } from 'expo-image'
-import httpService, { IMAGE_BASE } from '../../utils/httpService'
-import { useMutation, useQueryClient } from 'react-query'
-import { URLS } from '../../services/urls'
+import { View, Text } from "react-native";
+import React from "react";
+import Box from "../general/Box";
+import PrimaryButton from "../general/PrimaryButton";
+import CustomText from "../general/CustomText";
+import { useNavigation } from "@react-navigation/native";
+import { PageType } from "../../pages/login";
+import { ICommunity } from "../../models/Community";
+import { Image } from "expo-image";
+import httpService, { IMAGE_BASE } from "../../utils/httpService";
+import { useMutation, useQueryClient } from "react-query";
+import { URLS } from "../../services/urls";
+import useToast from "../../hooks/useToast";
 
 const CommunityListCard = (props: ICommunity) => {
   const { name, description, profile_image, id, is_member } = props;
   const navigation = useNavigation<PageType>();
   const queryClient = useQueryClient();
+  const toast = useToast();
 
   const { isLoading, mutate } = useMutation({
-    mutationFn: ()=> httpService.post(`${URLS.JOIN_COMMUNITY}/${id}`),
+    mutationFn: () => httpService.post(`${URLS.JOIN_COMMUNITY}/${id}`),
     onSuccess: (data) => {
-      alert(`You've successfully join ${name} community`);
-      queryClient.invalidateQueries(['getCommunities']);
-      navigation.navigate('community', { id, data: props })
+      // alert(`You've successfully join ${name} community`);
+      toast.show(`You've successfully join ${name} community`, {
+        type: "success",
+      });
+      queryClient.invalidateQueries(["getCommunities"]);
+      navigation.navigate("community", { id, data: props });
     },
     onError: () => {
-      alert('An error occured');
-    }
+      alert("An error occured");
+    },
   });
-
+  console.log(name, is_member);
   return (
-    <Box width={'100%'} height={100} flexDirection='row' justifyContent='space-between' alignItems='center' paddingHorizontal='m'   borderBottomColor='secondaryBackGroundColor' borderBottomWidth={2}>
-
-        <Box flexDirection='row' flex={0.6} alignItems='center'>
-          <Box height={30} width={30} borderRadius={15} bg='primaryColor' overflow='hidden'>
-            <Image source={{ uri: `${IMAGE_BASE}${profile_image}`}} contentFit='cover' style={{ width: '100%', height: '100%'}} />
-          </Box>
-
-          <Box marginLeft='s'>
-            <CustomText variant='subheader' fontSize={16}>{name}</CustomText>
-            <CustomText variant='body' fontSize={15}>
-              {description?.length > 20 ? description?.substring(0, 20) + '...' : description}
-            </CustomText>
-          </Box>
+    <Box
+      width={"100%"}
+      height={100}
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+      paddingHorizontal="m"
+      borderBottomColor="secondaryBackGroundColor"
+      borderBottomWidth={2}
+    >
+      <Box flexDirection="row" flex={0.6} alignItems="center">
+        <Box
+          height={30}
+          width={30}
+          borderRadius={15}
+          bg="primaryColor"
+          overflow="hidden"
+        >
+          <Image
+            source={{ uri: `${IMAGE_BASE}${profile_image}` }}
+            contentFit="cover"
+            style={{ width: "100%", height: "100%" }}
+          />
         </Box>
 
-        { is_member === 0 && (
-          <PrimaryButton title='Join' onPress={() => mutate()} height={35} width={70}></PrimaryButton>
-        )}
-        { is_member !== 0 && (
-          <PrimaryButton title='View' onPress={() => navigation.navigate('community', { id, data: props })} height={35} width={70}></PrimaryButton>
-        )}
-    </Box>
-  )
-}
+        <Box marginLeft="s">
+          <CustomText variant="subheader" fontSize={16}>
+            {name}
+          </CustomText>
+          <CustomText variant="body" fontSize={15}>
+            {description?.length > 20
+              ? description?.substring(0, 20) + "..."
+              : description}
+          </CustomText>
+        </Box>
+      </Box>
 
-export default CommunityListCard
+      {is_member === 0 && (
+        <PrimaryButton
+          title="Join"
+          onPress={() => mutate()}
+          height={35}
+          width={70}
+        ></PrimaryButton>
+      )}
+      {is_member !== 0 && (
+        <PrimaryButton
+          title="View"
+          onPress={() => navigation.navigate("community", { id, data: props })}
+          height={35}
+          width={70}
+        ></PrimaryButton>
+      )}
+    </Box>
+  );
+};
+
+export default CommunityListCard;
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,5 @@
+import Box from "../../components/general/Box";
+
+export default function Register() {
+  return <Box></Box>;
+}
