e7de0abe2f2e433173b306a1292b0a0d20ebac34
izuutech <izutechsofficial@gmail.com>
2023-12-04 16:38:15 +0100

    fix/after forget password navigate to right page

--- src/pages/password-reset/index.tsx
+++ src/pages/password-reset/index.tsx
@@ -1,43 +1,63 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useResetState } from './pages/state'
-import Email from './pages/email';
-import Verify from './pages/verify';
-import Reset from './pages/reset';
-import Box from '../../components/general/Box';
-import { Ionicons } from '@expo/vector-icons'
-import { useTheme } from '@shopify/restyle';
-import { Theme } from '../../theme';
-import CustomText from '../../components/general/CustomText';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../../navigation/MainNavigation';
+import { View, Text } from "react-native";
+import React, { useCallback } from "react";
+import { useResetState } from "./pages/state";
+import Email from "./pages/email";
+import Verify from "./pages/verify";
+import Reset from "./pages/reset";
+import Box from "../../components/general/Box";
+import { Ionicons } from "@expo/vector-icons";
+import { useTheme } from "@shopify/restyle";
+import { Theme } from "../../theme";
+import CustomText from "../../components/general/CustomText";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../../navigation/MainNavigation";
+import { useFocusEffect } from "@react-navigation/native";
 
-const ResetPassword = ({ navigation }: NativeStackScreenProps<RootStackParamList, 'reset-password'>) => {
-    const { stage } = useResetState((state) => state);
-    const theme = useTheme<Theme>();
+const ResetPassword = ({
+  navigation,
+}: NativeStackScreenProps<RootStackParamList, "reset-password">) => {
+  const { stage, setAll } = useResetState((state) => state);
+  const theme = useTheme<Theme>();
 
-    const switchPages = React.useCallback(() => {
-            switch(stage) {
-                case 1: {
-                    return <Email />
-                }
-                case 2: {
-                    return <Reset />
-                }
-                default: {
-                    return <Email />
-                }
-            }
-    }, [stage]);
+  const switchPages = React.useCallback(() => {
+    switch (stage) {
+      case 1: {
+        return <Email />;
+      }
+      case 2: {
+        return <Reset />;
+      }
+      default: {
+        return <Email />;
+      }
+    }
+  }, [stage]);
+
+  useFocusEffect(
+    useCallback(() => {
+      const unsubscribe = () => {
+        setAll({ stage: 1 });
+      };
+
+      return () => unsubscribe();
+    }, [])
+  );
   return (
-    <Box flex={1} backgroundColor='mainBackGroundColor' paddingHorizontal='m'>
-        <Box flexDirection='row' alignItems='center' paddingTop='l' flex={0.13}   >
-            <Ionicons onPress={() => navigation.goBack()} name='arrow-back-outline' size={30} color={theme.colors.textColor}  />
-            <CustomText variant='subheader' marginLeft='s' fontSize={23}>Reset Password</CustomText>
-        </Box>
-        {switchPages()}
+    <Box flex={1} backgroundColor="mainBackGroundColor" paddingHorizontal="m">
+      <Box flexDirection="row" alignItems="center" paddingTop="l" flex={0.13}>
+        <Ionicons
+          onPress={() => navigation.goBack()}
+          name="arrow-back-outline"
+          size={30}
+          color={theme.colors.textColor}
+        />
+        <CustomText variant="subheader" marginLeft="s" fontSize={23}>
+          Reset Password
+        </CustomText>
+      </Box>
+      {switchPages()}
     </Box>
-  )
-}
+  );
+};
 
-export default ResetPassword
+export default ResetPassword;
